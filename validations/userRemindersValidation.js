9880be95b8e55d83286ed1881f045beeb0116b77
HimanshuJustkeyless <himanshu.justkeyless@gmail.com>
2023-08-10 11:55:00 +0530

    staicker id number

--- validations/userRemindersValidation.js
+++ validations/userRemindersValidation.js
@@ -3,14 +3,14 @@ const { Joi } = require('celebrate');
 const userRemindersValidation = {
   Create: Joi.object().keys({
     user_id: Joi.string().required(),
-    sticker_id: Joi.string().required(),
+    sticker_id: Joi.number().required(),
     vehicle_number: Joi.string().required(),
     title: Joi.string().required(),
     expire_date: Joi.date().required(),
     reminder_date: Joi.date().required(),
   }),
   Get: Joi.object().keys({
-    sticker_id: Joi.string().required(),
+    sticker_id: Joi.number().required(),
   }),
   getUpcomingReminder: Joi.object().keys({
     user_id: Joi.string().required(),
