a08844ac298ff2516c62f4eac6fdd98370a17bbe
sandeep <s7895084627@gmail.com>
2025-03-31 16:38:02 +0530

    small changes

--- index.js
+++ index.js
@@ -22,8 +22,8 @@ app.use(express.static('public'));
 app.use(cors());
 app.use(upload());
 app.use(express.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(express.json());
+app.use(bodyParser.json({ limit: '50mb' }));
+// app.use(express.json());
 
 app.all('/api/v1/*', (req, res, next) => {
   const publicRoutes = ['auth/login', 'auth/register'];
